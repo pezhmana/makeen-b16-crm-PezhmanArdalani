30934cb677005ac9db8235b2e988e6e497775837
pezhmana <pejman.ardalani2@gmail.com>
2024-03-19 13:36:01 +0330

    edit route ha baraye controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\categoriesController;
+use App\Http\Controllers\ordersController;
+use App\Http\Controllers\postsController;
+use App\Http\Controllers\productsController;
+use App\Http\Controllers\UserController;
 use App\Http\Requests\categoriesCreateRequest;
 use App\Http\Requests\categoriesEditRequest;
 use App\Http\Requests\maghaleCreateRequest;
@@ -13,6 +18,7 @@ use App\Http\Requests\userEditRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use PHPUnit\TextUI\XmlConfiguration\Group;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,55 +38,19 @@ Route::get('/', function () {
 
 
 //users get routes
-Route::get('/users', function () {
-   $users = DB::table('users')->get();
-    return view('users.index' , ['users'=> $users]);
-});
 
-Route::get('/users/create', function () {
-    return view('users.create');
-});
+Route::get('/users', [UserController::class, 'index'])->name('users.index') ;
 
-Route::get('/users/edit/{id}', function ($id) {
-    $user = DB::table('users')->where('id' , $id)->get()->first();
-    return view('users.edit' , ['user'=>$user]);
+Route::get('/users/create',[UserController::class , 'createGet'])->name('users.createGet') ;
 
-});
+Route::get('/users/edit/{id}', [UserController::class , 'editGet'])->name('users.editGet') ;
 
 //user post routes
-Route::post('/users/create', function (userCreateRequest $request) {
-    DB::table('users')->insert([
-        'id'=>$request->id,
-        'name'=>$request->name,
-        'last_name'=>$request->last_name,
-        'phone'=>$request->phone,
-        'password'=>$request->password,
-        'country'=>$request->country,
-        'gender'=>$request->gender,
-        'description'=>$request->description,
-    ]);
-    return redirect('/users');
-});
+Route::post('/users/create', [UserController::class, 'createPost'])->name('users.createPost') ;
 
-route::post('/users/edit/{id}' , function (userEditRequest $request , $id){
-    DB::table('users')->where('id' , $id)->update([
-        'id'=>$request->id,
-        'name'=>$request->name,
-        'last_name'=>$request->last_name,
-        'phone'=>$request->phone,
-        'password'=>$request->password,
-        'country'=>$request->country,
-        'gender'=>$request->gender,
-        'description'=>$request->description,
-    ]);
-    return redirect('/users');
-});
+route::post('/users/edit/{id}' ,[UserController::class,'editPost'])->name('users.editPost') ;
 
-Route::delete('users/delete/{id}', function ($id) {
-    DB::table('users')->where('id',$id)->delete();
-    return redirect('/users');
-
-});
+Route::delete('users/delete/{id}',[UserController::class,'delete'])->name('users.delete') ;
 
 
 
@@ -90,161 +60,52 @@ Route::delete('users/delete/{id}', function ($id) {
 
 //products get routes
 
-Route::get('/products', function () {
-   $products = DB::table('products')->get();
-    return view('products.index' , ['products'=> $products]);
-});
+Route::get('/products',[productsController::class, 'index'])->name('products.index');
 
-Route::get('/products/create', function () {
-   $categories = DB::table('categories')->get();
-    return view('products.create' , ['categories'=>$categories]);
-});
+Route::get('/products/create',[productsController::class, 'createGet'])->name('products.createGet');
 
-Route::get('/products/edit/{id}', function ($id) {
-   $product =  DB::table('products')->where('id' , $id)->get()->first();
-   $categories = DB::table('categories')->get();
-    return view('products.edit' , ['product'=>$product] , ['categories'=>$categories]);
-});
-
-
-
+Route::get('/products/edit/{id}', [productsController::class, 'editGet'])->name('products.editGet');
 
 //products post routes
-Route::post('/products/create', function (productsCreateRequest $request) {
-    DB::table('products')->insert([
-        "id"=>$request->id,
-        "products_name"=>$request->products_name,
-        "colour"=>$request->colour,
-        "categories"=>$request->categories,
-        "stock"=>$request->stock,
-        "description"=>$request->description,
+Route::post('/products/create',[productsController::class, 'createPost'])->name('products.createPost');
 
-    ]);
-    return redirect('/products');
-});
+Route::post('/products/edit/{id}',[productsController::class, 'editPost'])->name('products.editPost');
 
-Route::post('/products/edit/{id}', function (productsEditRequest $request , $id) {
-    DB::table('products')->where('id',$id)->update([
-        "id"=>$request->id,
-        "products_name"=>$request->products_name,
-        "colour"=>$request->colour,
-        "categories"=>$request->categories,
-        "stock"=>$request->stock,
-        "description"=>$request->decription,
-    ]);
-    return redirect('/products');
-});
-
-Route::delete("/products/delete/{id}" , function ($id){
-    DB::table('products')->where('id', $id)->delete();
-    return redirect('products');
-
-});
+Route::delete("/products/delete/{id}" ,[productsController::class, 'delete'])->name('products.delete');
 
 
 
 
 //orders get routes
 
-Route::get('/orders', function () {
-  $orders = DB::table('orders')->get();
-    return view('orders.index' , ['orders'=>$orders]);
-});
+Route::get('/orders',[ordersController::class , "index"])->name('orders.index');
 
-Route::get('/orders/create', function () {
-    return view('orders.create');
-});
+Route::get('/orders/create',[ordersController::class , "createGet"])->name('orders.createGet');
 
-Route::get('/orders/edit/{id}', function ($id) {
-    $order = DB::table('orders')->where('id' , $id)->get()->first();
-        return view('orders.edit' , ['order'=>$order]);
-});
+Route::get('/orders/edit/{id}', [ordersController::class , "editGet"])->name('orders.editGet');
 
 //orders post routes
-Route::post('/orders/create', function (ordersCreateRequest $request) {
-    DB::table('orders')->insert([
-        'id'=>$request->id,
-        'name'=>$request->name,
-        'last_name'=>$request->last_name,
-        'phone'=>$request->phone,
-        'order_name'=>$request->order_name,
-        'number'=>$request->number,
-        'order_number'=>$request->order_number,
-        'price'=>$request->price,
-        'description'=>$request->description,
-    ]);
-    return redirect('/orders');
-});
+Route::post('/orders/create', [ordersController::class , "createPost"])->name('orders.createPost');
 
-Route::post('/orders/edit/{id}', function ($id , ordersEditRequest $request) {
-    DB::table('orders')->where('id',$id)->update([
-        'id'=>$request->id,
-        'name'=>$request->name,
-        'last_name'=>$request->last_name,
-        'phone'=>$request->phone,
-        'order_name'=>$request->order_name,
-        'number'=>$request->number,
-        'order_number'=>$request->order_number,
-        'price'=>$request->price,
-        'description'=>$request->description,
-    ]);
-    return redirect('/orders' );
-});
+Route::post('/orders/edit/{id}', [ordersController::class , "editPost"] )->name('orders.editPost');
 
-Route::delete('/orders/delete/{id}', function ($id) {
-    DB::table('orders')->where('id', $id)->delete();
-    return redirect('/orders');
-});
+Route::delete('/orders/delete/{id}', [ordersController::class , "delete"] )->name('orders.delete');
 
 
 //maghale get routes
 
-Route::get('/maghale', function () {
-    $maghale = DB::table('posts')->get();
-    return view('maghale.index' , ['maghale'=>$maghale]);
-});
+Route::get('/maghale', [postsController::class,'index'])->name('maghale.index');
 
-Route::get('/maghale/create', function () {
-    $categories = DB::table('categories')->get();
-    return view('maghale.create' , ['categories'=>$categories]);
-});
+Route::get('/maghale/create', [postsController::class,'createGet'])->name('maghale.createGet');
 
-Route::get('/maghale/edit/{id}', function ($id) {
-    $maghale = DB::table('posts')->where('id',$id)->get()->first();
-    $categories = DB::table('categories')->get();
-    return view('maghale.edit' , ['maghale'=>$maghale] , ['categories'=>$categories]);
-
-});
+Route::get('/maghale/edit/{id}', [postsController::class,'editGet'])->name('maghale.editGet');
 
 //maghale post routes
-Route::post('/maghale/create', function (maghaleCreateRequest $request) {
-    DB::table('posts')->insert([
-       'id'=>$request->id,
-       'categories'=>$request->categories,
-       'title'=>$request->title,
-       'writer'=>$request->writer,
-       'date'=>$request->date,
-       'source'=>$request->source,
-       'catID'=>$request->catID
-    ]);
-    return redirect('/maghale');
-});
+Route::post('/maghale/create', [postsController::class,'createPost'])->name('maghale.createPost');
 
-Route::post('/maghale/edit/{id}', function (maghaleEditRequest $request ,$id) {
-    DB::table('posts')->where('id',$id)->update([
-        'categories'=>$request->categories,
-       'title'=>$request->title,
-       'writer'=>$request->writer,
-       'date'=>$request->date,
-       'source'=>$request->source,
-    ]);
-    return redirect('/maghale');
-});
+Route::post('/maghale/edit/{id}', [postsController::class,'editPost'])->name('maghale.editPost');
 
-Route::delete('/maghale/delete/{id}', function ($id) {
-    DB::table('posts')->where('id',$id)->delete();
-    return redirect('/maghale');
-});
+Route::delete('/maghale/delete/{id}',[postsController::class,'delete'])->name('maghale.delete');
 
 
 
@@ -253,44 +114,19 @@ Route::delete('/maghale/delete/{id}', function ($id) {
 
 //categories get routes
 
-Route::get('/categories', function () {
-    $categories = DB::table('categories')->get();
-    return view('categories.index' , ['categories'=>$categories]);
-});
+Route::get('/categories', [categoriesController::class ,"index"])->name('categories.index');
 
-Route::get('/categories/create', function () {
-    return view('categories.create');
-});
+Route::get('/categories/create', [categoriesController::class ,"createGet"])->name('categories.createGet');
 
-Route::get('/categories/edit/{id}', function ($id) {
-    $categories = DB::table('categories')->where('id',$id)->get()->first();
-    return view('categories.edit' , ['categories'=>$categories]);
-});
+Route::get('/categories/edit/{id}',[categoriesController::class ,"editGet"])->name('categories.editGet');
 
 //categories post routes
 
-Route::post('/categories/create', function (categoriesCreateRequest $request) {
-    DB::table('categories')->insert([
-        'category_name'=>$request->category_name,
-        'supgroup'=>$request->supgroup,
-        'supgroup_name'=>$request->supgroup_name,
-    ]);
-    return redirect('/categories');
-});
+Route::post('/categories/create',[categoriesController::class ,"createPost"])->name('categories.createPost');
 
-Route::post('/categories/edit/{id}', function ($id , categoriesEditRequest $request) {
-    DB::table('categories')->where('id', $id)->update([
-        'category_name'=>$request->category_name,
-        'supgroup'=>$request->supgroup,
-        'supgroup_name'=>$request->supgroup_name
-    ]);
-    return redirect('/categories');
-});
+Route::post('/categories/edit/{id}',[categoriesController::class ,"editPost"])->name('categories.editPost');
 
-Route::delete('/categories/delete/{id}', function ($id) {
-DB::table('categories')->where('id', $id)->delete();
-    return redirect('/categories');
-});
+Route::delete('/categories/delete/{id}',[categoriesController::class ,"delete"])->name('categories.delete');
 
 
 
